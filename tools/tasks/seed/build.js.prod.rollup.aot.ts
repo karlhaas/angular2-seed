e5ae2db2885d51ce572db7c83870b8aa9b246b63
Karl Haas <karl.haas@cnc.io>
2017-02-11 15:31:18 +0100

    Upgrade seed and dependencies

--- tools/tasks/seed/build.js.prod.rollup.aot.ts
+++ tools/tasks/seed/build.js.prod.rollup.aot.ts
@@ -3,7 +3,7 @@ import * as gulpLoadPlugins from 'gulp-load-plugins';
 import { join } from 'path';
 
 import Config from '../../config';
-import { makeTsProject, TemplateLocalsBuilder } from '../../utils';
+import { makeTsProject } from '../../utils';
 
 const plugins = <any>gulpLoadPlugins();
 
@@ -29,7 +29,6 @@ export = () => {
     });
 
   return result.js
-    .pipe(plugins.template(new TemplateLocalsBuilder().build(), {interpolate: /<%=([\s\S]+?)%>/g}))
     .pipe(gulp.dest(Config.TMP_DIR))
     .on('error', (e: any) => {
       console.log(e);
